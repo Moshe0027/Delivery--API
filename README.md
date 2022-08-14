5078a9e416d112603826916e3af954ea80bdc1a0
moshe.f <Moshe0027@gmail.com>
2022-08-14 09:48:34 +0300

    update files

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Delivery-API
+
+## Install
+
+     npm install
+
+
+## Run
+
+     npm run dev
+
+## Get retrieve all today’s deliveries
+
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var formdata = new FormData();
+
+var requestOptions = {
+method: 'GET',
+headers: myHeaders,
+body: formdata,
+redirect: 'follow'
+};
+
+fetch("http://localhost:1337/deliveries/daily", requestOptions)
+.then(response => response.text())
+.then(result => console.log(result))
+.catch(error => console.log('error', error));
--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,27 @@
 import { Express, Request, Response } from 'express'
 import { searchTermSchema } from "./schema/address.schema";
+import { getTimeslotsSchema } from './schema/timeslots.schema';
+import { createDeliverySchema, deliverySchema } from './schema/delivery.schema';
+import { createAddressHandler } from './controller/address.controller';
+import { getTimeslotHandler } from './controller/timesolts.controller';
+import { createDeliveryHandler , deleteDeliveryHandler , dailyDeliveryHandler , weeklyDeliveryHandler ,completeDeliveryHandler} from './controller/delivery.controller';
 import validateResource from './middleware/validateResource';
-import {createAddressHandler } from './controller/address.controller';
 
 const routes = (app: Express) => {
 
     app.post('/resolve-address', [validateResource(searchTermSchema)], createAddressHandler);
+
+    app.post('/timeslots', [validateResource(getTimeslotsSchema)], getTimeslotHandler);
+
+    app.post('/deliveries', [validateResource(createDeliverySchema)], createDeliveryHandler);
     
-    app.delete('/deliveries/:delivery_id')
+    app.post('/deliveries/:delivery_id/complete', [completeDeliveryHandler])
     
+    app.delete('/deliveries/:delivery_id', deleteDeliveryHandler);
+
+    app.get('/deliveries/daily', dailyDeliveryHandler);
+
+    app.get('/deliveries/weekly', weeklyDeliveryHandler);
 
 }
 
